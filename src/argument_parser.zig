c5a69342e4befda8d09ec99175d67805717ffa0e
Diego Ugarte <diego.ugarte@outlook.com>
2022-03-05 20:26:08 +0900

    Update parser

--- src/argument_parser.zig
+++ src/argument_parser.zig
@@ -1,83 +1,70 @@
 const argparse = @import("argparse");
+const AppOption = argparse.AppOption;
+const AppPositional = argparse.AppPositional;
 
 pub const ArgumentParser = argparse.ArgumentParser(.{
     .app_name = "fpng",
     .app_description = "Floating-point number generator",
     .app_version = .{ .major = 0, .minor = 1, .patch = 0 },
-}, &[_]argparse.AppOptionPositional{
-    .{
-        .option = .{
-            .name = "double_precision",
-            .short = "-d",
-            .long = "--double-precision",
-            .description = "Use f64 instead of f32 for generating time series",
-        },
+}, &.{
+    AppOption{
+        .name = "double_precision",
+        .short = "-d",
+        .long = "--double-precision",
+        .description = "Use f64 instead of f32 for generating time series",
     },
-    .{
-        .option = .{
-            .name = "number",
-            .short = "-n",
-            .long = "--number",
-            .metavar = "N",
-            .description = "Generate time series using N as initial value",
-            .takes = 1,
-            .default = &.{"0.0"},
-        },
+    AppOption{
+        .name = "number",
+        .short = "-n",
+        .long = "--number",
+        .metavar = "N",
+        .description = "Generate time series using N as initial value",
+        .takes = 1,
+        .default = &.{"0.0"},
     },
-    .{
-        .option = .{
-            .name = "increment",
-            .short = "-i",
-            .long = "--increment",
-            .metavar = "N",
-            .description = "Increment by N at each step",
-            .takes = 1,
-            .default = &.{"0.0"},
-        },
+    AppOption{
+        .name = "increment",
+        .short = "-i",
+        .long = "--increment",
+        .metavar = "N",
+        .description = "Increment by N at each step",
+        .takes = 1,
+        .default = &.{"0.0"},
     },
-    .{
-        .option = .{
-            .name = "mu",
-            .short = "-m",
-            .long = "--mu",
-            .metavar = "μ",
-            .description = "Random noise mean value",
-            .takes = 1,
-            .default = &.{"0.0"},
-        },
+    AppOption{
+        .name = "mu",
+        .short = "-m",
+        .long = "--mu",
+        .metavar = "μ",
+        .description = "Random noise mean value",
+        .takes = 1,
+        .default = &.{"0.0"},
     },
-    .{
-        .option = .{
-            .name = "sigma",
-            .short = "-s",
-            .long = "--sigma",
-            .metavar = "σ",
-            .description = "Random noise standard deviation",
-            .takes = 1,
-            .default = &.{"0.0"},
-        },
+    AppOption{
+        .name = "sigma",
+        .short = "-s",
+        .long = "--sigma",
+        .metavar = "σ",
+        .description = "Random noise standard deviation",
+        .takes = 1,
+        .default = &.{"0.0"},
     },
-    .{
-        .option = .{
-            .name = "seed",
-            .long = "--seed",
-            .metavar = "N",
-            .description = "Seed for generating random numbers",
-            .takes = 1,
-        },
+    AppOption{
+        .name = "seed",
+        .long = "--seed",
+        .metavar = "N",
+        .description = "Seed for generating random numbers",
+        .takes = 1,
     },
-    .{
-        .positional = .{
-            .name = "length",
-            .metavar = "LENGTH",
-            .description = "Time series length",
-        },
+}, &.{
+    AppPositional{
+        .name = "length",
+        .metavar = "LENGTH",
+        .description = "Time series length",
     },
-    .{
-        .positional = .{
-            .name = "output",
-            .metavar = "OUTPUT",
-            .description = "Output file name",
-        },
+    AppPositional{
+        .name = "output",
+        .metavar = "OUTPUT",
+        .description = "Output file name",
     },
 });
